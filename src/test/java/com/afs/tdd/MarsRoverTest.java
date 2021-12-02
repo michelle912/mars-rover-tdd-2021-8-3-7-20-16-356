493fd88846a0c2afe1ed4f2f1baff5474bb8c201
michelle <81959777+michelle912@users.noreply.github.com>
2021-12-02 16:37:26 +0800

    add test case for moving to south

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -35,5 +35,18 @@ public class MarsRoverTest {
         assertEquals("1 0 E", result);
     }
 
+    @Test
+    public void should_move_towards_south_when_executeCommand_given_command_is_M_and_direction_is_S() {
+        // given
+        MarsRover marsRover = new MarsRover(0,0, "S");
+
+        // when
+        marsRover.executeCommand(MOVE);
+        String result = marsRover.getReport();
+
+        // then
+        assertEquals("0 -1 S", result);
+    }
+
 
 }
